4b7062e96fe0d584c9d82f7f6597f364efe31ae5
hexaredecimal <mfanakagama@gmail.com>
2026-06-12 11:46:24 +0200

    src: Fix deployment errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+      lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
